779d9fceab1c9bd9044451f62e810083018e261f
Praveen Kumar <praveen9166050@gmail.com>
2024-09-20 21:58:44 +0530

    Auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const CustomError = require("../utils/customError");
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+  try {
+    const authHeader = req.header['authorization'];
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new CustomError(403, "Unauthorized");
+    }
+    const token = authHeader.split(' ')[1];
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedData) {
+      throw new CustomError(403, "Unauthorized");
+    }
+    req.user = decodedData;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = auth;
